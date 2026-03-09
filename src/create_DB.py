ad3a60924e34d35ab1d6799f8ad92791dfb90808
Kasper Balsby <46810418+SqeEzyx@users.noreply.github.com>
2026-03-09 14:05:33 +0100

    Create the Database with tables
    Initiated without data for now.

--- src/create_DB.py
+++ src/create_DB.py
@@ -1,10 +1,39 @@
 import sqlite3
-
+import DB_commands as db
 # filename to form database
 file = "LetMilk.db"
 
 try:
-  conn = sqlite3.connect(file)
-  print("Database LetMilk.db formed.")
+  con = sqlite3.connect(file)
+  print(f"Database {file}.db formed.")
+
+  db.create_table(con, f"""CREATE TABLE IF NOT EXISTS Product_Table (
+        Product_ID INTEGER PRIMARY KEY,
+        Product_NAME TEXT NOT NULL,
+        Product_Brand TEXT NOT NULL,
+        Product_Category TEXT NOT NULL,
+        Product_Weight TEXT NOT NULL
+                                    );""")
+  db.create_table(con,"""CREATE TABLE IF NOT EXISTS Barcode_Table (
+        Barcode REAL NOT NULL,
+        Product_ID INTEGER PRIMARY KEY
+                                    );""")
+  db.create_table(con, f"""CREATE TABLE IF NOT EXISTS Storage_Table (
+        Product_ID INTEGER PRIMARY KEY,
+        Storage_ID REAL NOT NULL,
+        Stock REAL NOT NULL
+                                    );""")
+  db.create_table(con, f"""CREATE TABLE IF NOT EXISTS Price_Table (
+        Product_ID INTEGER PRIMARY KEY,
+        Price REAL NOT NULL
+                                    );""")
+  db.create_table(con, f"""CREATE TABLE IF NOT EXISTS Sales_Table (
+        Product_ID INTEGER PRIMARY KEY,
+        Sale_ID REAL NOT NULL,
+        Quantity REAL NOT NULL,
+        Time_Stamp TEXT NOT NULL
+                                    );""")
+  db.close_connection(con)
+
 except:
-  print("Database LetMilk.db not formed.")
+  print("Database not formed.")
