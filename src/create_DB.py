8c8dc9121398c154063e8c4db28324e60aed1aef
Kasper Balsby <46810418+SqeEzyx@users.noreply.github.com>
2026-03-09 14:16:01 +0100

    Create DataBase v0.2 09/03-2026

--- src/create_DB.py
+++ src/create_DB.py
@@ -12,28 +12,28 @@ try:
         Product_NAME TEXT NOT NULL,
         Product_Brand TEXT NOT NULL,
         Product_Category TEXT NOT NULL,
-        Product_Weight TEXT NOT NULL
+        Product_Weight REAL NOT NULL
                                     );""")
   db.create_table(con,"""CREATE TABLE IF NOT EXISTS Barcode_Table (
-        Barcode REAL NOT NULL,
+        Barcode INTEGER NOT NULL,
         Product_ID INTEGER PRIMARY KEY
                                     );""")
   db.create_table(con, f"""CREATE TABLE IF NOT EXISTS Storage_Table (
         Product_ID INTEGER PRIMARY KEY,
-        Storage_ID REAL NOT NULL,
-        Stock REAL NOT NULL
+        Storage_ID INTEGER NOT NULL,
+        Stock INTEGER NOT NULL
                                     );""")
   db.create_table(con, f"""CREATE TABLE IF NOT EXISTS Price_Table (
         Product_ID INTEGER PRIMARY KEY,
-        Price REAL NOT NULL
+        Price REAL NOT NULL,
+        Campagin_Date TEXT NOT NULL
                                     );""")
   db.create_table(con, f"""CREATE TABLE IF NOT EXISTS Sales_Table (
         Product_ID INTEGER PRIMARY KEY,
-        Sale_ID REAL NOT NULL,
-        Quantity REAL NOT NULL,
-        Time_Stamp TEXT NOT NULL
+        Sale_ID INTEGER NOT NULL,
+        Quantity INTEGER NOT NULL,
+        
                                     );""")
-  db.close_connection(con)
 
 except:
   print("Database not formed.")
