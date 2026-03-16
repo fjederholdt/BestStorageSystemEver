7a049e30b6776c2a10008461e91a7e1d613d327b
Kasper Balsby <46810418+SqeEzyx@users.noreply.github.com>
2026-03-16 14:09:22 +0100

    Made error handling for all functions
    Standard error handling.
    if con
     try

--- src/database_commands.py
+++ src/database_commands.py
@@ -57,9 +57,16 @@ def execute_single_query(con,sql_insert_query):
 def execute_multi_query(con,insert_sql):
     """Execute a list of SQL insert querys
     """
-    for (insert,data) in insert_sql:
-        execute_single_query(con,(insert,data))
-            
+    if con is not None:
+        try:
+            for (insert,data) in insert_sql:
+                execute_single_query(con,(insert,data))
+        except Exception as e:
+            print(f"Error getting table data: {e}")
+            return 0
+    else:
+        print("No connection to database")
+        return 0
 
 def all_columns(con):
     """Gives the dict tables with their columns
@@ -176,64 +183,104 @@ def get_table(con,table):
     """Gets the data from a specific table
     Returns all data conceerning the specific table
     """
-    data = []
-    cursor = get_cursor(con)
-    cursor.execute(f"SELECT * FROM {table}")
-    temp_data = cursor.fetchall()
-    for d in temp_data:
-        data.append(d[0])
+    if con is not None:
+        try:
+            data = []
+            cursor = get_cursor(con)
+            cursor.execute(f"SELECT * FROM {table}")
+            temp_data = cursor.fetchall()
+            for d in temp_data:
+                data.append(d[0])
+        except Exception as e:
+            print(f"Error getting table data: {e}")
+            return 0
+    else:
+        print("No connection to database")
+        return 0
     return data
 
 def get_column(con,table,column):
     """Gets data from a specific table, from a specific column
     Returns all data concerning the specific column
     """
-    data = []
-    cursor = get_cursor(con)
-    cursor.execute(f"SELECT {column} FROM {table}")
-    temp_data = cursor.fetchall()
-    for d in temp_data:
-        data.append(d[0])
+    if con is not None:
+        try:
+            data = []
+            cursor = get_cursor(con)
+            cursor.execute(f"SELECT {column} FROM {table}")
+            temp_data = cursor.fetchall()
+            for d in temp_data:
+                data.append(d[0])
+        except Exception as e:
+            print(f"Error getting column data: {e}")
+            return 0
+    else:
+        print("No connection to database")
+        return 0
     return data
 
 def get_id(con,table,column,id):
     """Gets data from specific table, from specific column, with specific id
     Returns all data concerning the specific id
     """
-    data = []
-    cursor = get_cursor(con)
-    cursor.execute(f"SELECT * FROM {table} WHERE {column} = '{id}'")
-    temp_data = cursor.fetchall()
-    for d in temp_data:
-        data.append(d)
+    if con is not None:
+        try:
+            data = []
+            cursor = get_cursor(con)
+            cursor.execute(f"SELECT * FROM {table} WHERE {column} = '{id}'")
+            temp_data = cursor.fetchall()
+            for d in temp_data:
+                data.append(d)
+        except Exception as e:
+            print(f"Error getting id data: {e}")
+            return 0
+    else:
+        print("No connection to database")
+        return 0
     return data
 
 
 def get_all_data(con):
     """Gets entire dataset from database
     """
-    total = {}
-    cursor = get_cursor(con)
-    schema = all_columns(con)
-    for table, column in schema.items():
-        for item in column:
-            cursor.execute(f"SELECT {item} FROM {table}")
-            data = cursor.fetchall()
-            cur_data = []
-            for i in data:
-                cur_data.append(i[0])
-            total[item] = cur_data
+    if con is not None:
+        try:
+            total = {}      
+            cursor = get_cursor(con)
+            schema = all_columns(con)
+            for table, column in schema.items():
+                for item in column:
+                    cursor.execute(f"SELECT {item} FROM {table}")
+                    data = cursor.fetchall()
+                    cur_data = []
+                    for i in data:
+                        cur_data.append(i[0])
+                    total[item] = cur_data
+        except Exception as e:
+            print(f"Error getting all data: {e}")
+            return 0
+    else:
+        print("No connection to database")
+        return 0
     return total
 
 def get_tables_and_columns(schema):
     """Gets tables and columns from schema
     Returns a list of tables and a dict of columns
     """
-    tables = []
-    columns = {}
-    for table, column in schema.items():
-        tables.append(table)
-        columns[table] = column
+    if schema is not None:
+        try:
+            tables = []
+            columns = {}
+            for table, column in schema.items():
+                tables.append(table)
+                columns[table] = column
+        except Exception as e:
+            print(f"Error extracting tables and columns: {e}")
+            return 0
+    else:
+        print("No Schema given")
+        return 0
     return tables, columns
 
 def get_column_data_type(con, table, column):
@@ -249,43 +296,76 @@ def get_column_data_type(con, table, column):
                     return col[2]
         except Exception as e:
             print(f"Error getting column datatype: {e}")
+    else:
+        print("No connection to database")
     
 def get_datatypes(con,table,columns): 
     """Get datatypes for all columns in a table
         Returns dict of columns with their datatype
     """
-    datatypes = {}
-    for column in columns:
-        datatypes[column] = get_column_data_type(con,table,column)
+    if con is not None:
+        try:
+            datatypes = {}
+            for column in columns:
+                datatypes[column] = get_column_data_type(con,table,column)
+        except Exception as e:
+            print(f"Error getting datatypes: {e}")
+            return 0
+    else:
+        print("No connection to database")
+        return 0
     return datatypes
 
 def get_primary_key(con, table):
     """Gets the primary key column (name) of a table
     """
-    cursor = get_cursor(con)
-    cursor.execute(f"PRAGMA table_info({table})")
-    for row in cursor.fetchall():
-        if row[5] == 1: # PK column has 1 in the 6th position of the PRAGMA output
-            return row[1]
+    if con is not None:
+        try:
+            cursor = get_cursor(con)
+            cursor.execute(f"PRAGMA table_info({table})")
+            for row in cursor.fetchall():
+                if row[5] == 1: # PK column has 1 in the 6th position of the PRAGMA output
+                    return row[1]
+        except Exception as e:
+            print(f"Error getting primary key: {e}")
+            return 0
+    else:
+        print("No connection to database")
+        return 0
     return "ID"
  
 def get_connected_tables(con, parent_table): # Find tables that have a foreign key referencing the parent_table
-    schema = all_columns(con)
-    reference = []
-    for table, columns in schema.items():
-        cursor = get_cursor(con)
-        cursor.execute(f"PRAGMA foreign_key_list({table})")
-        for row in cursor.fetchall():
-            if row[2] == parent_table:
-                reference.append((table, row[3], row[4])) # row[3] is the column in the child table that references the parent table, row[4] is the column in the parent table that is referenced
-       
+    if con is not None:
+        try:
+            schema = all_columns(con)
+            reference = []
+            for table, columns in schema.items():
+                cursor = get_cursor(con)
+                cursor.execute(f"PRAGMA foreign_key_list({table})")
+                for row in cursor.fetchall():
+                    if row[2] == parent_table:
+                        reference.append((table, row[3], row[4])) # row[3] is the column in the child table that references the parent table, row[4] is the column in the parent table that is referenced
+        except Exception as e:
+            print(f"Error getting connected tables: {e}")
+            return 0
+    else:
+        print("No connection to database")
+        return 0
     return reference
  
 def get_mathing_ids(con, table, col, val):
-    cursor = get_cursor(con)
-    pk_column = get_primary_key(con, table)
-    cursor.execute(f"SELECT {pk_column} FROM {table} WHERE {col} = ?", (val,))
-    ids = [row[0] for row in cursor.fetchall()]
+    if con is not None:
+        try:
+            cursor = get_cursor(con)
+            pk_column = get_primary_key(con, table)
+            cursor.execute(f"SELECT {pk_column} FROM {table} WHERE {col} = ?", (val,))
+            ids = [row[0] for row in cursor.fetchall()]
+        except Exception as e:
+            print(f"Error getting matching ids: {e}")
+            return 0
+    else:
+        print("No connection to database")
+        return 0
     return ids, pk_column
 
 # ----------------- RESERVED DATABASE -----------------
@@ -304,31 +384,54 @@ def connect_reserved_db():
 def create_transit_table(con,prim_key,data_type):
     """Creates the Transit Table in the database
     """
-    execute_single_query(con,f"""CREATE TABLE IF NOT EXISTS Transit_Table (
-        {prim_key} {data_type} PRIMARY KEY,
-        Status TEXT NOT NULL,
-        Quantity INTEGER NOT NULL,
-        From_Location TEXT NOT NULL,
-        To_Location TEXT NOT NULL
-                                    );""")
-    commit(con)
+    if con is not None:
+        try:
+            execute_single_query(con,f"""CREATE TABLE IF NOT EXISTS Transit_Table (
+                {prim_key} {data_type} PRIMARY KEY,
+                Status TEXT NOT NULL,
+                Quantity INTEGER NOT NULL,
+                From_Location TEXT NOT NULL,
+                To_Location TEXT NOT NULL);""")
+            commit(con)
+        except Exception as e:
+            print(f"Error creating Transit Table: {e}")
+            return 0
+    else:
+        print("No connection to database")
+        return 0
 
 def insert_transit_table(con,data):
     """Inserts the data into the Transit Table
     """
-    schema = all_columns(con)
-    for table,columns in schema.items():
-        if table == "Transit_Table":
-            #(sql, col_data) = get_insert_cols(table,columns,data)
-            execute_single_query(con,get_insert_cols(table,columns,data))
+    if con is not None:
+        try: 
+            schema = all_columns(con)
+            for table,columns in schema.items():
+                if table == "Transit_Table":
+                    execute_single_query(con,get_insert_cols(table,columns,data))
+        except Exception as e:
+            print(f"Error insert Transit Table data: {e}")
+            return 0
+    else:
+        print("No connection to database")
+        return 0
     return 1
 
 def update_transit(con,status,primary_key, primary_key_id):
     """Updates the status of the Transit Table
     """
-    for s in Status:
-        if s.value == status:
-            update(con, "Transit_Table", "Status", s.name, primary_key, primary_key_id)
+    if con is not None:
+        try:
+            for s in Status:
+                if s.value == status:
+                    update(con, "Transit_Table", "Status", s.name, primary_key, primary_key_id)
+                    return 1
+        except Exception as e:
+            print(f"Error updating Transit Table Status: {e}")
+            return 0
+    else:
+        print("No connection to database")
+        return 0
 
 def initiate_reserved_data(prim_key,prim_key_val,quantity,from_loc,to_loc):
     """Generates the initial data for the Transit Table
@@ -342,20 +445,25 @@ def reserve_data(con,prim_key,prim_key_val,quantity,from_loc,to_loc):
     """Reserves the data
     Done by generating and inserting into the Transit Table
     """
-    data = initiate_reserved_data(prim_key,prim_key_val,quantity,from_loc,to_loc)
+    if con is not None:
+        try: 
+            data = initiate_reserved_data(prim_key,prim_key_val,quantity,from_loc,to_loc)
 
-    if type(prim_key_val) is int:
-        data_type = "INTEGER"
-    elif type(prim_key_val) is str:
-        data_type = "TEXT"
-    elif type(prim_key_val) is float:
-        data_type = "REAL"
+            if type(prim_key_val) is int:
+                data_type = "INTEGER"
+            elif type(prim_key_val) is str:
+                data_type = "TEXT"
+            elif type(prim_key_val) is float:
+                data_type = "REAL"
+            else:
+                print("No datatype")
+                quit()
+
+            create_transit_table(con,prim_key,data_type)
+            insert_transit_table(con,data)
+        except Exception as e:
+            print(f"Error reserving data: {e}")
+            return 0
     else:
-        print("No datatype")
-        quit()
-
-    create_transit_table(con,prim_key,data_type)
-    insert_transit_table(con,data)
-
-con = connect_reserved_db()
-reserve_data(con,"Product_ID",111,6,"hero","mero")
+        print("No connection to database")
+        return 0
